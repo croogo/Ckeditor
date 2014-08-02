264b76bebe0b0c83bd2549312ba732763f77ebff
66d27b3ea53eda58ad7f71253dc2338b08efe4c4 <botrelli@gmx.com>
2014-08-02 16:31:48 -0400

    CS Fixes

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,10 +3,10 @@
 /**
  * Hook helper
  */
-foreach ((array)Configure::read('Wysiwyg.actions') as $action => $settings) {
-	if (is_numeric($action)) {
-		$action = $settings;
-	}
-	$actionE = explode('/', $action);
-	Croogo::hookHelper($actionE['0'], 'Ckeditor.Ckeditor');
+foreach ((array) Configure::read('Wysiwyg.actions') as $action => $settings) {
+    if (is_numeric($action)) {
+        $action = $settings;
+    }
+    $actionE = explode('/', $action);
+    Croogo::hookHelper($actionE['0'], 'Ckeditor.Ckeditor');
 }
